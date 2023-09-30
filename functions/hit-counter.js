36f83f79e9e1580767da7b5d3d75aec5f175753b
nila27 <68037709+nila27@users.noreply.github.com>
2023-09-30 22:30:48 +0900

    Delete functions directory

--- functions/hit-counter.js
+++
@@ -1,9 +0,0 @@
-let data = { visits: 0 };
-
-exports.handler = async (event, context) => {
-    data.visits += 1;
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-    };
-};
